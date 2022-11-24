79ed56502fe0362f1187bb72308d155cf4f4c450
sarah <sarah001160@gmail.com>
2022-11-24 22:29:56 +0800

    finished Aliens' animation

--- js/alljq.js
+++ js/alljq.js
@@ -2,6 +2,7 @@ $('document').ready(function(){
     $(window).scroll(function(){
         let scrollPos = $(window).scrollTop();//window滑行高度
         let windowHeight = $(window).height();//視窗高度
+        //let windowHeight - $(window).height()
         
         $('.jqAnimate').each(function(){ //選定的對象
             let thisPos = $(this).offset().top;
@@ -14,10 +15,24 @@ $('document').ready(function(){
         $('#AlienOne').each(function(){
             let thisPos = $(this).offset().top;
             if((scrollPos + windowHeight)>= thisPos){
-                $(this).addClass('animate')
+                $(this).addClass('animate');
+            }
+        });
+
+        $('#AlienTwo').each(function(){
+            let thisPos = $(this).offset().top;
+            if((scrollPos + windowHeight)>= thisPos){
+                $(this).addClass('animate');
             }
         })
 
+        $('#AlienThree').each(function(){
+            let thisPos = $(this).offset().top;
+            if((scrollPos + windowHeight)>thisPos){
+                $(this).addClass('animate');
+            }
+
+        })
        
 
     });
